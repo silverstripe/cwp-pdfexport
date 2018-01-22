0b2a675278185f205ddce3072ee0a7f570284a0c
Robbie Averill <robbie@silverstripe.com>
2018-01-23 11:04:35 +1300

    NEW Import PDF export functionality from BasePage in cwp/cwp

---
+++ tests/Extensions/PdfExportExtensionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CWP\PDFExport\Tests\Extensions;
+
+use CWP\CWP\PageTypes\BasePage;
+use CWP\PDFExport\Extensions\PdfExportExtension;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Dev\SapphireTest;
+
+class PdfExportExtensionTest extends SapphireTest
+{
+    protected static $fixture_file = 'PdfExportExtensionTest.yml';
+
+    protected static $required_extensions = [
+        BasePage::class => [
+            PdfExportExtension::class,
+        ],
+    ];
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Config::modify()
+            ->set(BasePage::class, 'pdf_export', true)
+            ->set(BasePage::class, 'generated_pdf_path', 'assets/_generated_pdfs');
+    }
+
+    public function testPdfFilename()
+    {
+        $page = $this->objFromFixture(BasePage::class, 'test-page-one');
+        $this->assertContains(
+            'assets/_generated_pdfs/test-page-one-1.pdf',
+            $page->getPdfFilename(),
+            'Generated filename for PDF'
+        );
+    }
+
+    public function testPdfLink()
+    {
+        $page = $this->objFromFixture(BasePage::class, 'test-page-one');
+        $this->assertContains('test-page-one/downloadpdf', $page->PdfLink(), 'Link to download PDF');
+    }
+
+    public function testHomePagePdfLink()
+    {
+        $page = $this->objFromFixture(BasePage::class, 'home-page');
+        $this->assertContains('home/downloadpdf', $page->PdfLink(), 'Link to download PDF');
+    }
+
+    public function testPdfLinkDisabled()
+    {
+        Config::modify()->set(BasePage::class, 'pdf_export', false);
+        $page = $this->objFromFixture(BasePage::class, 'test-page-one');
+        $this->assertFalse($page->PdfLink(), 'No PDF link as the functionality is disabled');
+    }
+}
